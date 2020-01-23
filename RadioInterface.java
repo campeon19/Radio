f5f30e1796002ffc11500c3d859924aa60d6fa5a
campeon19 <53914171+campeon19@users.noreply.github.com>
2020-01-22 22:16:05 -0600

    Codigo Comentado

--- RadioInterface.java
+++ RadioInterface.java
@@ -1,3 +1,9 @@
+/* Christian Daniel Perez De Leon (19710)
+ * Pablo Reyna (19822)
+ * Fecha de creacion: 18/01/2020
+ * Ultima edicion: 22/01/2020
+ */
+
 public interface RadioInterface{
 
     public String estacionActual();
